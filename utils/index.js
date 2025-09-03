80dca4b0c041bb302b16799f3332aee8a8302610
Moorent Pm <moorentpm@gmail.com>
2025-09-03 15:00:49 +0200

    Fix: Aggiunta utils e corretto Pagination per Next.js

---
+++ utils/index.js
@@ -0,0 +1,7 @@
+// Funzione per creare URL corretti per le pagine
+export const createPageUrl = (pageName) => {
+    // Converte il nome della pagina in minuscolo per l'URL
+    return `/${pageName.toLowerCase()}`;
+  };
+  
+  
